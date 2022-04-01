3b0442632dc865f472c9c86a13bcf67d065733a8
Zaino <hello@darrin.dev>
2022-04-01 14:14:16 -0700

    add main pages

---
+++ src/pages/community-projects.js
@@ -0,0 +1,21 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Container } from 'react-bootstrap'
+
+// Step 2: Define your component
+const CommunityProjectsPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <Container>
+          this is Comunity Projects
+      </Container>
+      <row>row</row>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default CommunityProjectsPage
