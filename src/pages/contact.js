8bc0a3fa979491082fca4ae5b7df184f2234ecae
Zaino <hello@darrin.dev>
2022-04-02 12:40:52 -0700

    update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,39 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Container } from 'react-bootstrap'
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+
+import NavGlobal from "../components/nav-global"
 
 // Step 2: Define your component
 const ContactPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Container>
-          this is Contact
-      </Container>
-      <row>row</row>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout>
+        <Seo title="Contact" />
+      <main>
+        <Row id="about">
+          <Col md className='col-1 d-flex justify-content-end'>
+            <NavGlobal />
+          </Col>
+          <Col md>
+            <div className='contact-content'>
+              <h3 className='mt-5 mb-3'>Contact Page</h3>
+              <p>We will put a form here.  
+              </p>
+              
+            </div>
+          </Col>
+          <Col md></Col>
+        </Row>
+      </main>
+    </Layout>
+    
   )
 }
 
