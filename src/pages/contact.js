8d193a0e61c860c0005753b034589ecd4d3ee723
Zaino <hello@darrin.dev>
2022-04-09 14:32:07 -0700

    update contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,8 +8,6 @@ import Seo from "../components/seo"
 
 import { Row } from 'react-bootstrap'
 import { Col } from 'react-bootstrap'
-import { Form } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
 
 //import NavGlobal from "../components/nav-global"
 import Moe from "../components/moe"
@@ -31,10 +29,15 @@ const ContactPage = () => {
             <div className='triptych-content'>
               <h2 className='triptych-title'>Contact</h2>
               <ContactForm />
+              <h3 className='mt-5 mb-3 invisible'>PLAYWRIGHT BIO</h3>
+              <p className='invisible'>Irony typewriter cliche, tofu lo-fi venmo hot chicken portland DIY fingerstache kitsch taxidermy pitchfork disrupt. Four dollar toast forage iPhone 90's pour-over. Bespoke pickled thundercats snackwave. Migas fingerstache raclette, four loko health goth gochujang beard bicycle rights freegan shabby chic. Normcore post-ironic umami cronut four loko authentic, celiac cornhole. Fanny pack williamsburg blog chia twee blue bottle poke. Butcher tattooed forage vape distillery, unicorn +1.
+              </p>
+             
+             
             </div>
             <div id="moe-footer"></div>
           </Col>
-          <Col md className='col-3'>
+          <Col md>
               <Dorie />
           </Col>
         </Row>
