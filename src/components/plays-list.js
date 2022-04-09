094f4e35ca2ff6eb4efee3b1b363f007c6d752f1
Zaino <hello@darrin.dev>
2022-04-09 14:46:24 -0700

    add plays and plays list

---
+++ src/components/plays-list.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+
+import { Link } from "gatsby"
+
+const PlaysList = () => (
+    <div id="plays-list" className='mt-5'>
+                <ul>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                  <li><h5><a href="#">Play</a></h5></li>
+                </ul>
+              </div>
+  );
+  
+  export default PlaysList;
--- src/pages/plays.js
+++ src/pages/plays.js
@@ -1,19 +1,48 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Container } from 'react-bootstrap'
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+
+//import NavGlobal from "../components/nav-global"
+import Moe from "../components/moe"
+import Dorie from "../components/dorie"
+import PlaysList from "../components/plays-list"
+
 
 // Step 2: Define your component
 const PlaysPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Container>
-          this is plays
-      </Container>
-      <row>row</row>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout>
+        <Seo title="Plays" />
+      <main>
+        <Row id="triptych">
+          <Col id="" md className='col-1'>
+            <Moe />
+          </Col>
+          <Col md>
+            <div className='triptych-content'>
+              <h2 className='triptych-title'>Plays</h2>
+              <PlaysList />
+              <p className='invisible'>Irony typewriter cliche, tofu lo-fi venmo hot chicken portland DIY fingerstache kitsch taxidermy pitchfork disrupt. Four dollar toast forage iPhone 90's pour-over. Bespoke pickled thundercats snackwave. Migas fingerstache raclette, four loko health goth gochujang beard bicycle rights freegan shabby chic. Normcore post-ironic umami cronut four loko authentic, celiac cornhole. Fanny pack williamsburg blog chia twee blue bottle poke. Butcher tattooed forage vape distillery, unicorn +1.
+              </p>
+              <p className='invisible'>Irony typewriter cliche, tofu lo-fi venmo hot chicken portland DIY fingerstache kitsch taxidermy pitchfork disrupt. Four dollar toast forage iPhone 90's pour-over. Bespoke pickled thundercats snackwave. Migas fingerstache raclette, four loko health goth gochujang beard bicycle rights freegan shabby chic. Normcore post-ironic umami cronut four loko authentic, celiac cornhole. Fanny pack williamsburg blog chia twee blue bottle poke. Butcher tattooed forage vape distillery, unicorn +1.
+              </p>
+            </div>
+            <div id="moe-footer"></div>
+          </Col>
+          <Col md>
+              <Dorie />
+          </Col>
+        </Row>
+      </main>
+    </Layout>
+    
   )
 }
 
