d33e00eea7634df632a6b7a09007dd757daa4f87
Zaino <hello@darrin.dev>
2022-04-09 17:20:58 -0700

    update plays template 2

---
+++ src/pages/plays-template-2.js
@@ -0,0 +1,64 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Container } from 'react-bootstrap'
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import NavGlobal from '../components/nav-global'
+
+//import NavGlobal from "../components/nav-global"
+
+// Step 2: Define your component
+const PlaysTemplatePage = () => {
+  return (
+    <Layout>
+    <Seo title="Play Title" />
+  <main>
+    <Row id="play">
+      <Col id="" md className='col-1'>
+        <NavGlobal/>
+      </Col>
+      <Col md>
+        <div className='play-content mb-5'>
+          <h3 className='play-title mt-5 mb-3'>A Christmas Carol</h3>
+          {/* Media Obect */ }
+          <div className="d-flex play-media-object">
+            <div className="flex-shrink-0 play-img-container">
+              <img className="align-self-start mr-3 play-img" src="https://via.placeholder.com/150" alt="Generic placeholder image"></img>
+            </div>
+            <div className="flex-grow-1 ms-3 play-description">
+            Thanks to their miserly stage manager, a down-on-its-luck theater company has to produce their annual Dickens play without a Scrooge (quit), a Tiny Tim (fired) or a chain for Marley’s Ghost (lost). How they find a way to make the show go on transforms them all as they invent their way through Christmas Past, Christmas Present and Christmas Future.  A fast, fresh, moving, and family-friendly 60 minute classic.  
+            </div>
+            
+          </div>
+          {/* Media Obect */ }
+          <div className="play-meta-container">
+            <p className="play-meta mt-5"><strong>World premiere:</strong> Mark Taper, 1977.</p>
+            <p className="play-meta mt-3"><strong>Productions include:</strong> The Cleveland Playhouse, National Theater of the Deaf, The Semafor Theater,  Prague.</p>
+            <p className="play-meta mt-3"><strong>Most Recent:</strong> The Alley Theater, Houston TX., 2021.</p>
+            <p className="play-meta mt-3"><strong>Press:</strong> <i>…the choice of the Alley Theater to do this script was a good one for where theatre is today. We are all coming back to our lives, picking up the pieces of where we left off before a pandemic separated us into isolation. Now the hearts that will remain are the hearts who have shown up again, today, without a chain, without lead characters, but with a willingness to figure it out together and in love. That is the spirit of theater.</i><strong>   Broadway World,  Dec. 2021</strong> </p>
+            <p className="play-meta mt-3"><strong>Publisher: Dramatists Play Service</strong></p>
+          </div>
+          {/* Additional Content */}
+          <div className="flex-shrink-0 play-img-container">
+              <img class="align-self-start mr-3 mt-5 play-img" src="https://via.placeholder.com/300" alt="Generic placeholder image"></img>
+          </div>
+          <h6 className="play-meta mt-3">A caption</h6>
+        </div>
+        
+      </Col>
+      <Col md>
+          col 2
+      </Col>
+    </Row>
+  </main>
+</Layout>
+
+  )
+}
+
+// Step 3: Export your component
+export default PlaysTemplatePage
---
+++ src/snippets/media-object.js
@@ -0,0 +1,11 @@
+{/* Media Obect */ }
+<div class="d-flex">
+<div class="flex-shrink-0">
+  <img class="align-self-start mr-3" src="https://via.placeholder.com/150" alt="Generic placeholder image"></img>
+</div>
+<div class="flex-grow-1 ms-3">
+This is some content from a media component. You can replace this with any content and adjust it as needed.
+</div>
+<a href="https://getbootstrap.com/docs/5.1/utilities/flex/#media-object">check out the documentation here</a>
+</div>
+{/* Media Obect */ }
