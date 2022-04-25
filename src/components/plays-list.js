b3fc63fc7e1592d7af5fd4e9a7055c90b86db165
Zaino <hello@darrin.dev>
2022-04-25 09:14:36 -0700

    add A Christmas Carol play

--- src/components/plays-list.js
+++ src/components/plays-list.js
@@ -4,22 +4,71 @@ import { Link } from "gatsby"
 
 const PlaysList = () => (
     <div id="plays-list" className='mt-5'>
-                <ul>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                  <li><h5><a href="#">Play</a></h5></li>
-                 
-                </ul>
-              </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="/a-christmas-carol" className=""><img className="align-self-end me-3 pl-img" src="https://zno.s3.us-west-1.amazonaws.com/a_christmas_carol.jpg" alt="A Christmas Carol"></img>A Christmas Carol</a>
+      </div>
+       {/* Play */}
+       <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Agnes Smedley, Our American Friend</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Daniel In Babylon</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Glass Mountain</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Mrs. California</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Prairie Fire</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Sex Sting</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Shiloh Rules</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Sister Kenny's Children</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Sisters of Peace</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>The Dyer's Hand</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>THe Mikado Project (with Ken Narasaki</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Tears of Rage</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Mimi's Guide</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>May</a>
+      </div>
+      {/* Play */}
+      <div className="media mb-3">
+        <a href="#" className=""><img className="align-self-end me-3 pl-img" src="https://via.placeholder.com/40" alt=""></img>Mary Dyer Hanged In Boston</a>
+      </div>
+    </div>
   );
   
   export default PlaysList;
